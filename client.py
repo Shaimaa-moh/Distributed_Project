ee526fdf3acfb87bd1d20226fd4fc149e1edcd6a
ranawalid <103863926+ranawalid@users.noreply.github.com>
2023-05-05 16:08:13 +0300

    networking
    create network class for connecting to our server and edit in client file
    
    Co-Authored-By: aya ahmad gamal <71007442+Aya-gamal2211@users.noreply.github.com>
    Co-Authored-By: Kareem Azab <67200068+KareemAzab21@users.noreply.github.com>
    Co-Authored-By: Shaimaa-moh <93445956+Shaimaa-moh@users.noreply.github.com>

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import pygame
 from player import Player
+from network import Network
 
 # window height and width
 width = 500
@@ -9,23 +10,28 @@ win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
 
 
-def redrawWindow():
+def redrawWindow(win,player,player2):
     
     # draw white window
     win.fill((255,255,255))
- 
+    player.draw(win)
+    player2.draw(win)
     pygame.display.update()
 
 
 def main():
     
     run = True
+    
+    n = Network()
+    p = n.getP()
    
     clock = pygame.time.Clock()
     
 #  dayman loop btt3ml fe pygame
     while run:
         clock.tick(60)
+        p2 = n.send(p)
   
 
         for event in pygame.event.get():
@@ -33,7 +39,7 @@ def main():
                 run = False
                 pygame.quit()
 
-     
-        redrawWindow()
+        p.move()
+        redrawWindow(win,p,p2)
 
 main()
