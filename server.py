0d2fc1435d85ac9b18716fa9cf375947de293f3e
Shaimaa-moh <19P7484@eng.asu.edu.eg>
2023-05-05 15:21:28 +0300

    Create server.py
    added server code responsible to create connection , bind the socket with our  ip and post no. , also receive data of player object and store their positions in list , and performs multi playing concept , if player  0  is playing reply  by  its position to player 1  and vice versa . also pickle library facilitates sending object of the players.

---
+++ server.py
@@ -0,0 +1,62 @@
+import socket
+from _thread import * 
+import sys
+from player import Player 
+import pickle
+server="192.168.1.6" #ip address of my device
+port=5555
+
+s=socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+
+try:
+    s.bind((server,port))
+    
+except socket.error as e:
+    str(e)
+    
+s.listen(2) #if we give it empty arguments yb2a mmkn y listen to multiple connections
+print("waiting for connection , Server Started")
+
+
+
+players = [ Player(0,0,50,50,(255,0,0)),Player(100,100,50,50,(0,0,255))] #store player object on the server
+
+def clientThread(conn , player ): #we want to have multiple connections done at once
+    conn.send(pickle.dumps(players[player])) # send initial player object
+    reply=""
+    while True:
+        try :
+            data = pickle.loads(conn.recv(2048))#longer the size of receiving msg , longer it can receive information4
+            # example :"45 , 67" -> (45,67)
+            players[player] =data
+            #pos[player]= data
+            #reply=data.decode("utf-8") #bec we are receiving encoded info
+            if not data:
+                print("Disconnected")
+                break
+            else: #if there are issues
+                if player==1: # if player 1 is playing send to him position of player 0
+                    reply =players[0]
+                    
+                else :
+                    reply =players[1]
+                    
+                print("received: ",data)
+                print("Sending :",reply)
+            
+            conn.sendall(pickle.dumps(reply))
+              
+        except:
+            break
+        
+    print("lost conection")
+    conn.close()        
+# server needs to keep track of position of the two players and how many players are connected
+currentPlayer = 0
+while True: # coninuously look for connections
+    conn,addr = s.accept() #accepts any upcoming connections
+    print("connected successfully to :", addr)
+    start_new_thread(clientThread,(conn , currentPlayer))
+    currentPlayer += 1
+    
+    
