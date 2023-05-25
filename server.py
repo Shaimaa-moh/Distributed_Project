71bcb5525bbe5977dcd4ce89cfea9605ceb49042
Kareem Azab <kaazab35@gmail.com>
2023-05-25 20:41:48 +0300

    reading highscore from the db

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import pickle
 import random
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-server = "192.168.1.10"  # ip address of my device
+server = "192.168.1.6"  # ip address of my device
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -33,40 +33,44 @@ try:
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
-db =client['Distributed_Project2023']
+db = client['Distributed_Project2023']
 collections = db['Players']
-players = [Player(250, 479, "./images/car.png"), Player(400, 479,"./images/divo.png")]  # store player object on the server
+players = [Player(250, 479, "./images/car.png"), Player(400, 479,
+                                                        "./images/divo.png")]  # store player object on the server
 s.listen(2)
 game_end = False
 collide = [False, False]
 
-score=[0,0]  #for two players
+score = [0, 0]  # for two players
+
 
 def clientThread(conn, player):  # we want to have multiple connections done at once
     conn.send(pickle.dumps(players[player]))  # send initial player object
-    
+
     reply = ""
     global score
     global game_end
     global collide
-    
+
     global currentPlayer
-    global collections  #like table players
-    
+    global collections  # like table players
+
     victory = False
 
     while True:
         try:
             # longer the size of receiving msg , longer it can receive information4
-            data    = pickle.loads(conn.recv(4096)) #object from player
-            document = collections.find_one({'username': data["Username"]}) #see username in db or not
+            data = pickle.loads(conn.recv(4096))  # object from player
+            document = collections.find_one(
+                {'username': data["Username"]})  # see username in db or not
             if document is None:
-                collections.insert_one({'username':data["Username"],'highscore':0})
-            
+                collections.insert_one(
+                    {'username': data["Username"], 'highscore': 0})
+
             # example :"45 , 67" -> (45,67)
             players[player] = data["Player"]
             collide[player] = data["Crashed"]
-            score  [player]  =  data["score"]
+            score[player] = data["score"]
             # pos[player]= data
             # reply=data.decode("utf-8") #bec we are receiving encoded info
             if not data:
@@ -83,10 +87,11 @@ def clientThread(conn, player):  # we want to have multiple connections done at 
                     reply = {
                         "Oponent": players[0],
                         "Connections": currentPlayer,
-                        "car1": car1_x,   #collision cars
+                        "car1": car1_x,  # collision cars
                         "car2": car2_x,
                         "End_Game": game_end,
-                        "Victory": victory
+                        "Victory": victory,
+                        "Highscore": document['highscore'],
                     }
 
                 else:
@@ -101,7 +106,8 @@ def clientThread(conn, player):  # we want to have multiple connections done at 
                         "car1": car1_x,
                         "car2": car2_x,
                         "End_Game": game_end,
-                        "Victory": victory
+                        "Victory": victory,
+                        "Highscore": document['highscore'],
                     }
 
                 print("received: ", data)
@@ -109,10 +115,13 @@ def clientThread(conn, player):  # we want to have multiple connections done at 
 
             conn.sendall(pickle.dumps(reply))
             if collide[0] == True and collide[1] == True:
-                document_final = collections.find_one({'username': data["Username"]}) #see username in db or not
-                if (document_final['highscore'] < score[player]): # to take score of player 1 or 2 
+                document_final = collections.find_one(
+                    {'username': data["Username"]})  # see username in db or not
+                # to take score of player 1 or 2
+                if (document_final['highscore'] < score[player]):
                     update = {'$set': {'highscore': score[player]}}
                     collections.update_one(document_final, update)
+                conn.sendall(pickle.dumps(reply))
                 currentPlayer -= 1
                 break
 
@@ -120,7 +129,7 @@ def clientThread(conn, player):  # we want to have multiple connections done at 
             break
 
     print("lost conection")
-    
+
     conn.close()
 
 
