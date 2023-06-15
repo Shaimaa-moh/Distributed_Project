f37265e436b599ee2278eac22017d2be74508935
Shaimaa-moh <19P7484@eng.asu.edu.eg>
2023-06-16 02:33:55 +0300

    update running two instances from ec2 and making the two diff servers run on
    Co-Authored-By: aya ahmad gamal <71007442+Aya-gamal2211@users.noreply.github.com>
    Co-Authored-By: ranawalid <103863926+ranawalid@users.noreply.github.com>
    Co-Authored-By: Kareem Azab <67200068+KareemAzab21@users.noreply.github.com>

--- Chat/chattest.py
+++ Chat/chattest.py
@@ -5,7 +5,7 @@ from tkinter import font
 from tkinter import ttk
 
 PORT = 3000
-SERVER = "13.53.49.203"
+SERVER = "16.171.58.116"
 ADDRESS = (SERVER, PORT)
 # Create a new client socket
 # and connect to the server
