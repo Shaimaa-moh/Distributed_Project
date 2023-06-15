9a815aabcd0aca4e38ae2cafafa4cc7dbe4759cf
Shaimaa-moh <19P7484@eng.asu.edu.eg>
2023-06-16 02:57:41 +0300

    Update serv.py
    Co-Authored-By: aya ahmad gamal <71007442+Aya-gamal2211@users.noreply.github.com>
    Co-Authored-By: ranawalid <103863926+ranawalid@users.noreply.github.com>
    Co-Authored-By: Kareem Azab <67200068+KareemAzab21@users.noreply.github.com>

--- Chat/serv.py
+++ Chat/serv.py
@@ -194,7 +194,7 @@ class Server:
         # self.clientList.add('Mary')
         # self.clientList.add('bob')
         # ipconfig on commandline
-        self.host = '16.171.58.116'
+        self.host = '172.31.28.254'
         self.socket = socket.socket(
             socket.AF_INET, socket.SOCK_STREAM)  # IP uses 4 bytes
     # It doesnt block other processes if it waits to accept a connection
