e72f9cd1c0f61d54d96d580a2372899691490cb9
Shaimaa-moh <19P7484@eng.asu.edu.eg>
2023-06-16 00:47:28 +0300

    update
    Co-Authored-By: ranawalid <103863926+ranawalid@users.noreply.github.com>

--- Chat/chattest.py
+++ Chat/chattest.py
@@ -5,7 +5,7 @@ from tkinter import font
 from tkinter import ttk
 
 PORT = 3000
-SERVER = "13.51.167.69"
+SERVER = "172.31.23.232"
 ADDRESS = (SERVER, PORT)
 # Create a new client socket
 # and connect to the server
