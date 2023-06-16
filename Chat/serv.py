c767b6c8900aacf1c8a6deba5c66a4245e5192f4
ranawalid <103863926+ranawalid@users.noreply.github.com>
2023-06-16 16:42:11 +0300

    update ip 2
    second trial

--- Chat/serv.py
+++ Chat/serv.py
@@ -194,7 +194,7 @@ class Server:
         # self.clientList.add('Mary')
         # self.clientList.add('bob')
         # ipconfig on commandline
-        self.host = '13.51.106.13'
+        self.host = '13.53.82.16'
         self.socket = socket.socket(
             socket.AF_INET, socket.SOCK_STREAM)  # IP uses 4 bytes
     # It doesnt block other processes if it waits to accept a connection
