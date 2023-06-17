6e02211ea45d800d5a9a2f5747c291ad5256b5ec
Kareem Azab <kaazab35@gmail.com>
2023-06-17 23:00:41 +0300

    deploying server

--- Chat/chattest.py
+++ Chat/chattest.py
@@ -5,7 +5,7 @@ from tkinter import font
 from tkinter import ttk
 
 PORT = 3000
-SERVER = "44.212.43.71"
+SERVER = "16.170.162.38"
 ADDRESS = (SERVER, PORT)
 # Create a new client socket
 # and connect to the server
